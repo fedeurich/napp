aa01bd79814f276b72002cc5b268f41c767b207d
Jorge Federico Urich <federicourichlc@gmail.com>
2024-01-27 19:40:21 -0300

    Mejora index

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,21 +3,21 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: "password",
+    password: "Jfu36647067",
     database: "ecommerce_dbtest",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
-    password: "password",
+    password: "Jfu36647067",
     database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: "password",
+    password: "Jfu36647067",
     database: "database_production",
     host: "127.0.0.1",
     dialect: "mysql",
