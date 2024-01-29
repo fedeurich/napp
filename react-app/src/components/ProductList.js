17295f4b381b055d50cd3f019f32d045b60d15ae
Gino Lencina <84538965+GinoL221@users.noreply.github.com>
2024-01-29 19:13:20 -0300

    imagenes react

--- react-app/src/components/ProductList.js
+++ react-app/src/components/ProductList.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:3031/api/products')
-      .then(response => response.json())
-      .then(data => {
+    fetch("http://localhost:3031/api/products")
+      .then((response) => response.json())
+      .then((data) => {
         setProducts(data.products);
         setLoading(false);
       })
-      .catch(error => console.error('Error fetching products:', error));
+      .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
   return (
@@ -21,33 +21,38 @@ const ProductList = () => {
         <p>Cargando productos...</p>
       ) : (
         <div className="row">
-          {products.map(product => (
-          <div key={product.IDProduct} className="col-lg-4">
-            <div className="card shadow mb-4 ">
-              <div className="card-header py-3">
-                <h5 className="m-0 font-weight-bold text-gray-800">{product.NameProduct}</h5>
-              </div>
-              <div className="card-body">
-                <p className="h5 mb-0 font-weight-bold text-gray-800">ID: {product.IDProduct}</p>
-                <p className="h5 mb-0 font-weight-bold text-gray-800">Precio: ${product.Price}</p>
-                {/* Accede al nombre de la categoría correctamente */}
-                <p className="h5 mb-0 font-weight-bold text-gray-800">
-                  Categoría: {product.Category|| 'Sin categoría'}
-                </p>
-                <p className="h5 mb-0 font-weight-bold text-gray-800">
-                  Descripción: {product.DescriptionProduct}
-                </p>
-                {/* Muestra la imagen (asegúrate de tener la URL correcta) */}
-                <img
-                  src={`http://localhost:3031/img/api/${product.Image}`}
-                  alt={product.NameProduct}
-                  className="img-fluid rounded"
-                />
+          {products.map((product) => (
+            <div key={product.IDProduct} className="col-lg-4">
+              <div className="card shadow mb-4 ">
+                <div className="card-header py-3">
+                  <h5 className="m-0 font-weight-bold text-gray-800">
+                    {product.NameProduct}
+                  </h5>
+                </div>
+                <div className="card-body">
+                  <p className="h5 mb-0 font-weight-bold text-gray-800">
+                    ID: {product.IDProduct}
+                  </p>
+                  <p className="h5 mb-0 font-weight-bold text-gray-800">
+                    Precio: ${product.Price}
+                  </p>
+                  {/* Accede al nombre de la categoría correctamente */}
+                  <p className="h5 mb-0 font-weight-bold text-gray-800">
+                    Categoría: {product.Category || "Sin categoría"}
+                  </p>
+                  <p className="h5 mb-0 font-weight-bold text-gray-800">
+                    Descripción: {product.DescriptionProduct}
+                  </p>
+                  {/* Muestra la imagen (asegúrate de tener la URL correcta) */}
+                  <img
+                    src={`http://localhost:3031/img/products/${product.Image}`}
+                    alt={product.NameProduct}
+                    className="img-fluid rounded "
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-
+          ))}
         </div>
       )}
     </div>
