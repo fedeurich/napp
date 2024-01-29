32cb7e2e369d1f2a59d300533ed42daaa57e7dfb
Gino Lencina <84538965+GinoL221@users.noreply.github.com>
2024-01-29 12:30:10 -0300

    correccion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: null,
+    password: "password",
     database: "ecommerce_dbtest",
     host: "127.0.0.1",
     dialect: "mysql",
