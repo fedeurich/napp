998d8b71c45e8beaf6b0c18ae348e3b94b97b43f
Jorge Federico Urich <federicourichlc@gmail.com>
2024-04-17 19:55:03 -0300

    pal reedit

--- src/controllers/clients/getClientById.js
+++ src/controllers/clients/getClientById.js
@@ -1,21 +1,25 @@
-const { Client } = require("../../database/models");
+const { Client } = require("../../database/models"); // Asegúrate de importar el modelo User desde el archivo correcto
 const path = require("path");
 
 const getClientById = async (req, res) => {
+  const ruta = path.join(__dirname, "../../views/clients/clientDetail.ejs");
+  const { id } = req.params;
+
   try {
-    const clientId = req.params.id;
-    const client = await Client.findByPk(clientId);
+    // Realiza una consulta a la base de datos para obtener el usuario por ID
+    const client = await Client.findByPk(id);
 
     if (!client) {
+      // Si el usuario no existe, renderiza una vista de error 404
       return res.render(path.join(__dirname, "../../views/404NotFound"), {
-        message: "Client not found",
+        message: "User not found",
       });
     }
 
-    const ruta = path.join(__dirname, "../../views/clients/clientDetail.ejs");
+    // Renderiza la vista con los datos del usuario
     res.render(ruta, { client });
   } catch (error) {
-    console.error("Error al obtener detalles del client:", error);
+    console.error("Error al obtener cliente por ID:", error);
     res.status(500).send("Error interno del servidor");
   }
 };
